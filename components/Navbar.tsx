97872f740491df063704c020d23deda5d3f81d17
thetajwar2003 <rahmant7@bxscience.edu>
2021-02-24 22:11:30 -0500

    added toast messages

---
+++ components/Navbar.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Navbar() {
+  const user = null;
+  const username = null;
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+        {/* user is signed in and has username */}
+        {username && (
+          <>
+            <li>
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* user not signed in or has no username */}
+        {!username && (
+          <>
+            <li>
+              <Link href="/enter">
+                <button className="btn-blue">Log in</button>
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,14 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import toast from "react-hot-toast";
 
 import Loader from "../components/Loader";
 
 export default function Home() {
   return (
     <div>
-      {/* <Link
-        prefetch={false}
-        href={{
-          pathname: "/[username]",
-          query: { username: "testuser123" },
-        }}
-      >
-        <a>Test profile</a>
-      </Link> */}
-      <Loader show={true} />
+      <button onClick={() => toast.success("hello toast!")}>Toast me</button>
     </div>
   );
 }
